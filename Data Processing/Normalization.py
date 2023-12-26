2f4440a924ee58031df52479a4bded11086acfa6
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-26 16:45:27 +0000

    Update Normalization.py

--- Data Processing/Normalization.py
+++ Data Processing/Normalization.py
@@ -7,3 +7,8 @@ bins = pd.cut(df['b'],4)
 # discretize the attribute into 4 bins (equal-width)
 bins = pd.qcut(df['b'],4)
 # discretize the attribute into 4 bins (equal-depth)
+
+dfO['Home_or_restaurant']=df['Home_or_restaurant'].apply(lambda x: 'C' if x != 'A' and x != 'B' else x)
+# replace all value other than 'A' or 'B' with 'C'
+dfO['Home_or_restaurant']=dfO['Home_or_restaurant'].map({'home': 0, 'restaurant': 1})
+# replace muti-type of values with a map
