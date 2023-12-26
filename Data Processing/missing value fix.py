5a365869b3258a9d9fbb46961f694fd8f33e9d43
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-26 16:17:45 +0000

    Update missing value fix.py

--- Data Processing/missing value fix.py
+++ Data Processing/missing value fix.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
+df['Name'].isna()
+# assign all missing value row of the column with 1, others with 0 (work with np.NaN)
+df['Name'].isna().sum()
+# get the number of missing value in this column
+
 data = data.fillna(data.mean())
 data = data.fillna(data.median())
 data = data.fillna(data.mode())
