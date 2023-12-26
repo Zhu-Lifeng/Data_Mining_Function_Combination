d95e4fb637346a1bb500b7dfbd8dea03a7ca6107
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-26 16:10:53 +0000

    Update plots.py

--- Basic Function/plots.py
+++ Basic Function/plots.py
@@ -2,3 +2,11 @@ import seaborn as sns
 sns.pairplot(df)
 # seaborn plot matirx of all numeric value in the table
 import matplotlib.pyplot as plt
+
+import pandas as pd
+df['Year'].hist(bins=5)
+# histogram of the column
+df['Year'].boxplot()
+# boxplot of the column
+df.boxplot(figsize=(20,3))
+# boxplot of all numeric columns in the table (better have similar resolvation)
