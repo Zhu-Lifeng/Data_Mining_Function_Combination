809fb4018b5b98758704bb370cc94b39dd563b87
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-26 16:04:47 +0000

    Update missing value fix.py

--- Data Processing/missing value fix.py
+++ Data Processing/missing value fix.py
@@ -4,3 +4,10 @@ data = data.fillna(data.mean())
 data = data.fillna(data.median())
 data = data.fillna(data.mode())
 # fill the missing value with the mean/median/mode of the column (or any fixed value)
+# work with np.NaN
+# work for table data (replace a column with a fixed column)
+
+data =data.dropna()
+# delete all the missing value rows
+# work with np.NaN
+# work for 1-dimensional data
