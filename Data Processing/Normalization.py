e193e09617ac588c396a92f912ca718cc9dc9b63
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-26 16:32:22 +0000

    Update Normalization.py

--- Data Processing/Normalization.py
+++ Data Processing/Normalization.py
@@ -1,3 +1,9 @@
 import pandas as pd
 Z = (df['a']-df['a'].mean())/df.['a'].std()
 # Z-score
+
+
+bins = pd.cut(df['b'],4)
+# discretize the attribute into 4 bins (equal-width)
+bins = pd.qcut(df['b'],4)
+# discretize the attribute into 4 bins (equal-depth)
