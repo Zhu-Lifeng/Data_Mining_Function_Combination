c5559cd37618b9ede3f5489f2181f123f4c1cf4f
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-26 16:57:07 +0000

    Update plots.py

--- Basic Function/plots.py
+++ Basic Function/plots.py
@@ -2,6 +2,8 @@ import seaborn as sns
 sns.pairplot(df)
 # seaborn plot matirx of all numeric value in the table
 import matplotlib.pyplot as plt
+plt.scatter(d['x'],d['y'],c=d['Type'],marker=markerTypes[d['Type']],s=60)
+# Scatter with different color & marker for different types
 
 import pandas as pd
 df['Year'].hist(bins=5)
