d47b9f6255af1d1d506e9431182c11ca2cc56950
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-27 13:43:07 +0000

    Update MDS.py

--- Unsupervised Learning/Dimensionality Reduction/MDS.py
+++ Unsupervised Learning/Dimensionality Reduction/MDS.py
@@ -1,5 +1,7 @@
 from sklearn.manifold import MDS
 
 embedding = MDS(n_components=2)
-Xp = embedding.fit_transform(df.drop('Tag'))
+Xp = embedding.fit_transform(df.drop(['Tag']))
 df_projection = pd.DataFrame({'x': Xp[:, 0], 'y': Xp[:, 1],'Tag': df['Tag']})
+# multidimensional scaling, better use a standardized matrix as the input
+# output a matrix contains a 2-dimensional point for each sample
