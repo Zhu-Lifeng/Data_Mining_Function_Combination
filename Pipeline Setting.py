9030a3b339389af0e2a39e39f15e8da22db19d70
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-27 14:59:43 +0000

    Update Pipeline Setting.py

--- Pipeline Setting.py
+++ Pipeline Setting.py
@@ -5,4 +5,5 @@ from sklearn.neighbors import KNeighborsClassifier
 knn_pipe = make_pipeline(StandardScaler(), KNeighborsClassifier(n_neighbors=1))
 
 knn_pipe.fit(X_train, y_train)
+
 print('Test dataset accuracy: {0}.'.format(knn_pipe.score(X_test, y_test)))
