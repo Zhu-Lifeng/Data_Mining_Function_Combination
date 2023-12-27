010fdde0d1bd53c2ddb11be46de197f7eeb64fda
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-27 15:03:37 +0000

    Update Cross Validation.py

--- Validation/Cross Validation.py
+++ Validation/Cross Validation.py
@@ -11,3 +11,16 @@ print('Average accuracy across folds (k = 15): {0}.'.format(result_15['test_scor
       
 knn_5.fit(X_train, y_train)
 print('Test dataset accuracy (k = 5): {0}.'.format(knn_5.score(X_test, y_test)))
+
+
+from sklearn.model_selection import GridSearchCV
+# The class GridSearchCV offers a convenient way to choose hyperparameters based on cross-validation
+parameters = {'n_neighbors': [1, 3, 5, 7, 9]}
+
+knn = KNeighborsClassifier()
+knn_cv = GridSearchCV(knn, parameters, cv=5)
+knn_cv.fit(X_train, y_train)
+
+print('Best hyperparameter setting: {0}.'.format(knn_cv.best_estimator_))
+print('Average accuracy across folds of best hyperparameter setting: {0}.'.format(knn_cv.best_score_))
+print('Test dataset accuracy of best hyperparameter setting: {0}.'.format(knn_cv.score(X_test, y_test)))
