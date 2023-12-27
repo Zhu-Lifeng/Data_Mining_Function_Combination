be293703fb1105881490b1394bf7341b8131b324
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-27 14:57:15 +0000

    Update KNeighborsClassifier.py

--- Supervised Learning/Classification/KNeighborsClassifier.py
+++ Supervised Learning/Classification/KNeighborsClassifier.py
@@ -5,3 +5,6 @@ knn.fit(X, y)
 # the model itself
 print('Training dataset accuracy: {0}.'.format(knn.score(X, y)))
 # knn.score is the function to compute the accuracy
+y_pred=knn.predict(X)
+mistake=np.nonzero(y_pred != y)[0]
+# get the indices of misclassified samples
