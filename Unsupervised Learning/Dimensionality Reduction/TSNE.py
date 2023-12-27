c1a3b3dbc1c099e471ec7630c3245af43e174c90
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-27 13:45:40 +0000

    Update TSNE.py

--- Unsupervised Learning/Dimensionality Reduction/TSNE.py
+++ Unsupervised Learning/Dimensionality Reduction/TSNE.py
@@ -1,5 +1,6 @@
 from sklearn.manifold import TSNE
 
 embedding = TSNE(n_components=2, perplexity=100)
-Xp = embedding.fit_transform(X)
-df_projection = pd.DataFrame({'x': Xp[:, 0], 'y': Xp[:, 1],'': parental_education_sorted})
+Xp = embedding.fit_transform(df.drop(['Tag']))
+df_projection = pd.DataFrame({'x': Xp[:, 0], 'y': Xp[:, 1],'Tag': df['Tag']})
+# t-distributed stochastic neighbour embedding (TSNE)
