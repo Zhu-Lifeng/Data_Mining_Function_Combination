e2db1592cf0ed94dc90076f54b161b2e49745371
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-27 13:04:31 +0000

    Update Normalization.py

--- Data Processing/Normalization.py
+++ Data Processing/Normalization.py
@@ -1,7 +1,7 @@
 import pandas as pd
+import numpy as np
 Z = (df['a']-df['a'].mean())/df.['a'].std()
 # Z-score
-
 
 bins = pd.cut(df['b'],4)
 # discretize the attribute into 4 bins (equal-width)
@@ -12,3 +12,9 @@ dfO['Home_or_restaurant']=df['Home_or_restaurant'].apply(lambda x: 'C' if x != '
 # replace all value other than 'A' or 'B' with 'C'
 dfO['Home_or_restaurant']=dfO['Home_or_restaurant'].map({'home': 0, 'restaurant': 1})
 # replace muti-type of values with a map
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X=scaler.fit_transform(X.to_numpy())
+# X must only contain numeric features
+# generate standardized features, standardize each feature individually
