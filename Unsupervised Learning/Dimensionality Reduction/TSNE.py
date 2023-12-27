77b7c55a8bca01cc5e3cc4dd7420f47299e90d23
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-27 13:46:02 +0000

    Update TSNE.py

--- Unsupervised Learning/Dimensionality Reduction/TSNE.py
+++ Unsupervised Learning/Dimensionality Reduction/TSNE.py
@@ -4,3 +4,5 @@ embedding = TSNE(n_components=2, perplexity=100)
 Xp = embedding.fit_transform(df.drop(['Tag']))
 df_projection = pd.DataFrame({'x': Xp[:, 0], 'y': Xp[:, 1],'Tag': df['Tag']})
 # t-distributed stochastic neighbour embedding (TSNE)
+# better use a standardized matrix as the input
+# output a matrix contains a 2-dimensional point for each sample
