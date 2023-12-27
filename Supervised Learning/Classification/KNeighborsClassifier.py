a31283a1c88474adb2727819e7eee926dabd3418
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-27 14:54:44 +0000

    Update KNeighborsClassifier.py

--- Supervised Learning/Classification/KNeighborsClassifier.py
+++ Supervised Learning/Classification/KNeighborsClassifier.py
@@ -2,4 +2,6 @@ from sklearn.neighbors import KNeighborsClassifier
 
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(X, y)
+# the model itself
 print('Training dataset accuracy: {0}.'.format(knn.score(X, y)))
+# knn.score is the function to compute the accuracy
