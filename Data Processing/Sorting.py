133fe242ae708e04a4e335ae69e024e27666d3ab
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-27 12:28:11 +0000

    Create Sorting.py

---
+++ Data Processing/Sorting.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df.sort_values(by='Age', ascending = False)
+# sort the table by the feature 'Age', ascending = False => big number at the top / True=> small number at the buttom
+
