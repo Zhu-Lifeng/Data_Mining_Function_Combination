4c1a4e0a58e056a27824a24459b4cfaef0c4b34a
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-12-27 14:12:50 +0000

    Update Heatmap.py

--- Visualisation/Heatmap.py
+++ Visualisation/Heatmap.py
@@ -3,4 +3,15 @@ import seaborn as sns
 sns.heatmap(dist,square=True,xticklabels=False, yticklabels=False,cmap='Blues')
 # heatmap, work for distance matrix
 
-import matplotlib as plt
+import matplotlib.pyplot as plt
+plt.imshow(dist,cmap='Blues',aspect='equal',interpolation='nearest')
+plt.colorbar()
+plt.xticks([])
+plt.yticks([])
+# heatmap, work for distance matrix, use nearest neighbour interpolation (mutiple squares)
+
+plt.imshow(dist,cmap='Blues',aspect='equal',interpolation='bilinear')
+plt.colorbar()
+plt.xticks([])
+plt.yticks([])
+# heatmap, work for distance matrix, use bilinear interpolation (change smoothly)
